296baec2906b0a98f31774976b686b0de2ace152
Jônatas Ribas <91293805+JonatasRibas@users.noreply.github.com>
2024-02-15 16:19:10 -0300

    commit

--- server.js
+++ server.js
@@ -1,19 +1,29 @@
 const express = require('express')
+const session = require('express-session')
 const app = express()
 const PORT = process.env.PORT || 3000
 
 app.use(express.urlencoded({ extended: true }))
 app.use(express.static('public'))
+app.use(
+  session({ secret: 'mysecret', resave: false, saveUninitialized: false })
+)
 
 app.get('/', (req, res) => {
+  if (req.session.emailSent === undefined) {
+    req.session.emailSent = false
+  }
+
   res.sendFile(__dirname + '/public/index.html')
 })
 
 app.post('/submit', (req, res) => {
   const email = req.body.email
   console.log('Email recebido:', email)
-  // Aqui você pode salvar o email em um banco de dados ou fazer qualquer outra operação desejada
-  res.send('Email recebido com sucesso!')
+
+  req.session.emailSent = true
+
+  res.redirect('/')
 })
 
 app.listen(PORT, () => {
